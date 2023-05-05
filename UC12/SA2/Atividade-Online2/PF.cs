992a14d1d68975e1e3d04b37b9029729be09e3d9
Arnaldo F. Costa <arnaldo.ferreira.costa@gmail.com>
2023-05-05 09:31:23 -0300

    UC12 SA2 Atividade-Online2

---
+++ UC12/SA2/Atividade-Online2/PF.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atividade_Online2
+{
+    public class PF
+    {
+        public string? nome { get; set; }
+    }
+}
---
+++ UC12/SA2/Atividade-Online2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Atividade_Online2;
+
+PF pf = new PF();
+PJ pj = new PJ();
+
+string database = "./Database/Arnaldo.txt";
+
+Console.WriteLine("Nome: ");
+pf.nome = "Arnaldo";
+
+Console.WriteLine("Razão Social: ");
+pj.razaoSocial = "Arnaldo";
+
+
+using (StreamWriter sw = new StreamWriter(database))
+{
+    sw.WriteLine($"Nome: {pf.nome}");
+    sw.WriteLine($"Razão Social: {pj.razaoSocial}");
+
+}
+
+Console.WriteLine("Dados cadastrados com sucesso!");
+Console.ReadLine();
