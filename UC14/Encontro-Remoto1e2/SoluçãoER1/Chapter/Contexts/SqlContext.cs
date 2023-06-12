97f155bc5d6400dc8309c2924603150dca00fae5
Arnaldo F. Costa <arnaldo.ferreira.costa@gmail.com>
2023-06-12 11:08:48 -0300

    UC14 Encontro-Remoto 1 e 2

---
+++ UC14/Encontro-Remoto1e2/SoluçãoER1/Chapter/Contexts/SqlContext.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() { }
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data source = ARNALDO\\SQL_NOLD; initial catalog = Chapter; Integrated Security = true;");
+
+                // Exemplo 1 de string de conexão:
+                // User ID=sa; Password=admin; Server=localhost; Database=Chapter;-
+                // + Trusted_Connection = false;
+                // Exemplo 2 de string de conexão:
+                //Data Source=SQL_NOLD\\SQLEXPRESS;Initial catalog=Chapter;Integrated Security=true
+                //Server = localhost\\SQLEXPRESS; Database=Chapter; Trusted_Connection = True;
+            }
+        }
+        public DbSet<Livro> Livros { get; set; }
+
+
+    }
+}
---
+++ UC14/Encontro-Remoto1e2/SoluçãoER1/Chapter/Controllers/LivroController.cs
@@ -0,0 +1,108 @@
+﻿using Chapter.Models;
+using Chapter.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Livro livrobuscado = _livroRepository.BuscarPorId(id);
+                if (livrobuscado == null)
+                {
+                    return NotFound();
+                }
+                {
+                    return Ok(livrobuscado);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(Livro li)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(li);
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Excluir(int id)
+        {
+
+            try
+            {
+                _livroRepository.Excluir(id);
+
+                return Ok("Livro excluido com sucesso!");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Atualizar(int id, Livro li)
+        {
+
+            try
+            {
+                _livroRepository.Atualizar(id, li);
+
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+    }
+
+}
+
