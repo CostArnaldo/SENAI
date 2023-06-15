bb98ca6989209d2b07fc6769fe877c674e85ef7c
Arnaldo F. Costa <arnaldo.ferreira.costa@gmail.com>
2023-06-15 10:05:24 -0300

    UC14 Encontro-Remoto3

---
+++ UC14/ER3/SoluçãoER1/Chapter/Contexts/SqlContext.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+public class SqlContext : DbContext
+{
+    public SqlContext() { }
+    public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { 
+            optionsBuilder.UseSqlServer();
+
+            // Exemplo 1 de string de conexão:
+            // User ID=sa; Password=admin; Server=localhost; Database=Chapter;-
+            // + Trusted_Connection = false;
+            // Exemplo 2 de string de conexão:
+            //Data Source=SQL_NOLD\\SQLEXPRESS;Initial catalog=Chapter;Integrated Security=true
+            //Server = localhost\\SQLEXPRESS; Database=Chapter; Trusted_Connection = True;
+            
+    }
+    public DbSet<Livro> Livros { get; set; }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+
+
+}
+}
---
+++ UC14/ER3/SoluçãoER1/Chapter/Repositories/LivroRepository.cs
@@ -0,0 +1,55 @@
+﻿using Chapter.Contexts;
+using Chapter.Models;
+
+namespace Chapter.Repositories
+{
+public class LivroRepository
+{
+    private readonly SqlContext _context;
+
+    public LivroRepository(SqlContext context)
+    {
+        _context = context;
+    }
+
+    public List<Livro> Listar()
+    {
+        return _context.Livros.ToList();
+    }
+
+    public Livro BuscarPorId(int id)
+    {
+        return _context.Livros.Find(id);
+    }
+
+    public void Cadastrar(Livro li)
+    {
+        _context.Livros.Add(li);
+        _context.SaveChanges();
+    }
+
+    public void Atualizar(int id, Livro li)
+    {
+        Livro livroBuscado = _context.Livros.Find(id);
+
+        if (livroBuscado != null)
+        {
+            livroBuscado.Titulo = li.Titulo;
+            livroBuscado.Disponivel = li.Disponivel;
+            livroBuscado.QuantidadePaginas = li.QuantidadePaginas;
+
+            _context.Livros.Update(livroBuscado);
+
+            _context.SaveChanges();
+        }
+    }
+
+    public void Excluir(int id)
+    {
+        Livro li = _context.Livros.Find(id);
+        _context.Livros.Remove(li);
+        _context.SaveChanges();
+    }
+
+}
+}
