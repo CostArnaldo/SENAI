f6f6162ab19b1f30f8514c84fb29ecfa073a7343
Arnaldo F. Costa <arnaldo.ferreira.costa@gmail.com>
2023-06-26 21:59:02 -0300

    UC11

--- UC11/ER1/src/test/java/TesteCursoSenai.java
+++ UC11/ER1/src/test/java/TesteCursoSenai.java
@@ -1,5 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,9 +14,20 @@ public class TesteCursoSenai {
 	public void ConfigurarTeste() {
 	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\ChromeDriver\\chromedriver.exe");
 	driver = new ChromeDriver();
-	driver.manage().window().maximize()
-;	driver.get("http://www.google.com");
+	driver.manage().window().maximize();
+	driver.get("http://www.google.com");
+	driver.findElement(By.className("gLFyf"));
+	driver.findElement(By.className("gLFyf")).sendKeys("senai informatica");
+	driver.findElement(By.className("gLFyf")).sendKeys(Keys.ENTER);
+	driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div[1]/a/h3")).click();
+	
 	}
 	@Test
-		public void TesteN() {}
+		public void TesteN() {
+	
+	driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("excel");
+	driver.findElement(By.id("Busca1-btnBusca")).click();
+	}
+	
+	
 }
